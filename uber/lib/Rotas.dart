569b72766135066a5ec6db484129d0578c8b1786
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2021-12-04 19:46:08 -0300

    Configurando tela para cadastro de usuário.

--- uber/lib/Rotas.dart
+++ uber/lib/Rotas.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:uber/telas/Cadastro.dart';
 import 'package:uber/telas/Home.dart';
+import 'package:uber/telas/PainelMotorista.dart';
+import 'package:uber/telas/PainelPassageiro.dart';
 
 class Rotas{
   static Route<dynamic> gerarRotas( RouteSettings settings){
     switch(settings.name){
       case "/": return MaterialPageRoute(builder: (_) => Home());
       case "/cadastro": return MaterialPageRoute(builder: (_) => Cadastro());
+      case "/painel-motorista": return MaterialPageRoute(builder: (_) => PainelMotorista());
+      case "/painel-passageiro": return MaterialPageRoute(builder: (_) => PainelPassageiro());
       default: _erroRota();
     }
   }
--- uber/lib/telas/Cadastro.dart
+++ uber/lib/telas/Cadastro.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:uber/model/Usuario.dart';
 
 class Cadastro extends StatefulWidget {
 
@@ -10,7 +13,59 @@ class _CadastroState extends State<Cadastro> {
   TextEditingController _controllerNome = TextEditingController();
   TextEditingController _controllerEmail = TextEditingController();
   TextEditingController _controllerSenha = TextEditingController();
-  bool _tipoUsuarioPassageiro = false;
+  bool _tipoUsuario = false;
+  String _mensagemErro = "";
+
+  _validarCampos(){
+    String nome = _controllerNome.text;
+    String email = _controllerEmail.text;
+    String senha = _controllerSenha.text;
+
+    if(nome.isNotEmpty){
+      if(email.isNotEmpty && email.contains("@")){
+        if(senha.isNotEmpty && senha.length > 6){
+            Usuario usuario = Usuario();
+            usuario.nome = nome;
+            usuario.email = email;
+            usuario.senha = senha;
+            usuario.tipoUsuario = usuario.verificaTipoUsuario(_tipoUsuario);
+            _cadastrarUsuario(usuario);
+        }else{
+          setState(() {
+            _mensagemErro = "Preencha a senha! Digite mais de 6 caracteres.";
+          });
+        }
+      }else{
+        setState(() {
+          _mensagemErro = "Preencha um E-mail válido";
+        });
+      }
+    }else{
+      setState(() {
+        _mensagemErro = "Preencha o Nome";
+      });
+    }
+  }
+
+  _cadastrarUsuario(Usuario usuario){
+    FirebaseAuth auth = FirebaseAuth.instance;
+    Firestore db = Firestore.instance;
+    auth.createUserWithEmailAndPassword(email: usuario.email, password: usuario.senha)
+        .then((firebaseUser){
+          db.collection("usuarios")
+              .document(firebaseUser.user.uid)
+              .setData(usuario.toMap());
+          //Redirecionar para o painel do usuário de acordo com o tipo
+      switch(usuario.tipoUsuario){
+        case "motorista":
+          Navigator.pushNamedAndRemoveUntil(context, "/painel-motorista", (_) => false);
+          break;
+        case "passageiro":
+          Navigator.pushNamedAndRemoveUntil(context, "/painel-passageiro", (_) => false);
+          break;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,10 +126,10 @@ class _CadastroState extends State<Cadastro> {
                   children: [
                     Text("Passageiro"),
                     Switch(
-                      value: _tipoUsuarioPassageiro,
+                      value: _tipoUsuario,
                       onChanged: (bool valor){
                         setState(() {
-                          _tipoUsuarioPassageiro = valor;
+                          _tipoUsuario = valor;
                         });
                       },
                     ),
@@ -84,7 +139,9 @@ class _CadastroState extends State<Cadastro> {
                 Padding(
                   padding: EdgeInsets.only(top: 16, bottom: 10),
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      _validarCampos();
+                    },
                     child: Text(
                       "Cadastrar",
                       style: TextStyle(
@@ -99,7 +156,7 @@ class _CadastroState extends State<Cadastro> {
                 ),
                 Padding(
                   padding: EdgeInsets.only(top: 16),
-                  child: Center(child: Text("Erro!", style: TextStyle(color: Colors.red, fontSize: 20),)),
+                  child: Center(child: Text(_mensagemErro, style: TextStyle(color: Colors.red, fontSize: 20),)),
                 )
               ],
             ),
